4c2ca507e98b5408e591a828f90920a23b8c6318
Kaori Morikawa <morikawa_kaori@cyberagent.co.jp>
2018-05-31 10:16:04 +0900

    add example from 46 to 48

---
+++ gobyexample/47_epoch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+
+	now := time.Now()
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+
+	mills := nanos / 1000000
+	fmt.Println(secs)
+	fmt.Println(mills)
+	fmt.Println(nanos)
+
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
+}
